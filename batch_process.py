69c619f4878c433f4e325ba08f2c9702fb36ae34
jtjee <jee@ieee.org>
2021-01-21 22:30:04 -0800

    Changed the way files are parsed

--- batch_process.py
+++ batch_process.py
@@ -2,43 +2,25 @@ import os
 import pandas as pd
 import time
 import sys
+import io
 
-def strippify(filename):
-    f = open(filename)
-    text = f.read()
-    f.close()
-    nsplit = text.split('\n')
-    f = open(filename.replace('.txt', '_stripped.csv'), 'w')
-    f.close()
-    for n in nsplit:
-        if len(n.split('\t'))==13:
-            f = open(filename.replace('.txt', '_stripped.csv'), 'a')
-            f.write((n.split('\t')[0]
-                        + ',' + n.split('\t')[2]
-                        + ',' + n.split('\t')[4]
-                        + ',' + n.split('\t')[7]
-                        + ',' + n.split('\t')[8] + '\n'))
-            f.close()
-
-def batch_strippify(folder):
+def batch_convert_csv(folder):
+    i = 0
+    first = True
     for root, dirs, files in os.walk(folder):
         for file in files:
             if file.endswith(".txt"):
-                strippify(os.path.join(root, file))
-
-def batch_convert_csv(folder):
-    df = 0
-    i=0
-    for root, dirs, files in os.walk(folder):
-        for file in files:
-            if file.endswith(".csv"):
-                try:
-                    d = pd.read_csv(os.path.join(root, file))
-                    df = pd.concat([df, d], ignore_index=True)
-                except:
-                    df = pd.read_csv(os.path.join(root, file))
-            i+=1
-    df.to_csv(folder+'/DNA_extraction_data.csv')
+                text = open(os.path.join(root, file)).read()
+                textn = text.split('\n\n')
+                data = io.StringIO(textn[0])
+                if first:
+                    df = pd.read_csv(data, sep="\t")
+                    first = False
+                else:
+                    df = pd.concat([df, pd.read_csv(data, sep="\t")], ignore_index=True)
+                i += 1
+    df_filtered = df[['Sample ID', 'Date ', 'ng/ul ', '260/280 ', '260/230 ']]
+    df_filtered.to_csv(folder + '/data.csv', index=False)
     return i
 
 if __name__ == '__main__':
@@ -46,9 +28,7 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
         print('ERROR: Please only specify one argument')
         sys.exit()
-
-    start = time.time()
     folder = sys.argv[1]
-    batch_strippify(folder)
+    start = time.time()
     i = batch_convert_csv(folder)
     print('Converted {} Files in {} seconds'.format(i, round(time.time()-start,2)))
